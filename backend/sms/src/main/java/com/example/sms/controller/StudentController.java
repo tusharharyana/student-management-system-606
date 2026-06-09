9f6ab3131ebb51c6bcc6cb1fd589857bb1a80e47
tusharharyana <tusharkashyap203@gmail.com>
2026-06-09 14:42:22 +0530

    Code seperated for learning purpose

---
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -0,0 +1,51 @@
+package com.example.sms.controller;
+
+import com.example.sms.model.Student;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.sms.service.StudentService;
+
+@RestController
+@RequestMapping("/students")
+@CrossOrigin("*")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public ArrayList<Student> getStudents() {
+
+    ArrayList<Student> students = new ArrayList<>();
+
+        students.add(new Student(1, "John Doe", "Mca"));
+        students.add(new Student(2, "Jane Smith", "Bca"));
+        students.add(new Student(3, "Alice Johnson", "Mca"));
+
+        return students;
+    
+    }
+
+    @GetMapping("/bca")
+    public List<Student> getBcaStudents(){
+        return getStudents().stream()
+                .filter(student -> "Bca".equals(student.getCourse()))
+                .collect(Collectors.toList());
+    }
+
+
+    @GetMapping("/count")
+    public int countStudents(){
+        return service.getStudentCount();
+    }
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return service.getStudentInfo();
+    }
+
+}
---
+++ backend/sms/src/main/java/com/example/sms/service/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.sms.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public String getStudentInfo(){
+        return "Service is working!";
+    }
+
+    public Integer getStudentCount() {
+
+        String sql = "SELECT COUNT(*) FROM student";
+
+        return jdbcTemplate.queryForObject(sql,Integer.class);
+
+    }
+}

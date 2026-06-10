ce1fd1f1509ad979b4e5d222b84e0f0d563d9dd5
tusharharyana <tusharkashyap203@gmail.com>
2026-06-10 16:19:21 +0530

    Code prettier added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 function App() {
-
   const [students, setStudents] = useState([]);
   const [count, setCount] = useState(0);
 
@@ -9,10 +8,7 @@ function App() {
   const [course, setCourse] = useState("");
 
   const getStudents = async () => {
-
-    const response = await fetch(
-      "http://localhost:8080/students"
-    );
+    const response = await fetch("http://localhost:8080/students");
 
     const data = await response.json();
 
@@ -20,13 +16,11 @@ function App() {
   };
 
   const getBCAStudents = async () => {
-
-    const response = await fetch( "http://localhost:8080/students/bca");
+    const response = await fetch("http://localhost:8080/students/bca");
     const data = await response.json();
 
     setStudents(data);
   };
-
 
   const fetchTotalStudentCount = async () => {
     const response = await fetch("http://localhost:8080/students/count");
@@ -37,9 +31,9 @@ function App() {
     const response = await fetch("http://localhost:8080/students", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, course })
+      body: JSON.stringify({ name, course }),
     });
 
     if (response.ok) {
@@ -47,54 +41,42 @@ function App() {
     } else {
       alert("Failed to register student.");
     }
-    
   };
-
 
   return (
     <div style={{ padding: "40px" }}>
+      <button onClick={getStudents}>Get Students</button>
 
-      <button onClick={getStudents}>
-        Get Students
-      </button>
-
-      <button onClick={getBCAStudents}>
-         Show BCA students
-      </button>
+      <button onClick={getBCAStudents}>Show BCA students</button>
 
       <ul>
-        {students.map(student => (
+        {students.map((student) => (
           <li key={student.id}>
-              {student.name} - {student.course}
+            {student.name} - {student.course}
           </li>
         ))}
       </ul>
 
-
-      <button onClick={fetchTotalStudentCount}>
-        Get Total Student Count
-      </button>
+      <button onClick={fetchTotalStudentCount}>Get Total Student Count</button>
 
       <p>Total students: {count}</p>
 
-
       <h1>Student Registration Form</h1>
-      <input 
-        type="text" 
-        placeholder="Name" 
-        value={name} 
-        onChange={(e) => setName(e.target.value)} 
-        />
-
-      <input 
+      <input
         type="text"
-        placeholder="Course" 
-        value={course} 
-        onChange={(e) => setCourse(e.target.value)} 
-        />
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Course"
+        value={course}
+        onChange={(e) => setCourse(e.target.value)}
+      />
 
       <button onClick={addStudent}>Register</button>
-
     </div>
   );
 }
